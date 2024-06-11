8f4a904d35ad193d87f22303bd74b926c4085785
Darman <darman1407@gmail.com>
2024-06-11 15:07:39 +0700

    Menambahkan File index2.php

---
+++ index2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index 2 GIT</title>
+</head>
+<body>
+    <h1>Latihan Remote Github - GIT</h1><br>
+    <h1>Kelas 22 MI A</h1>
+</body>
+</html>
